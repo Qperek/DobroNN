74d783bbeb77425663ad0883882b2d029a2e2a99
Maciej Dobrowolski <maciekdgg@gmail.com>
2018-05-13 17:35:31 +0200

    bsaic UI implementatio

--- Magisterka/MainWindow.xaml.cs
+++ Magisterka/MainWindow.xaml.cs
@@ -23,6 +23,7 @@ namespace Magisterka
     public partial class MainWindow : Window
     {
         private NeuralNetworkManager nnManager;
+        private List<TextBox> layersTextBoxes;
 
         public MainWindow()
         {
@@ -30,7 +31,7 @@ namespace Magisterka
 
             nnManager = new NeuralNetworkManager(new SigmoidFunction(1), 784, 30, 10);
             
-            TestNeuralNetworkManager(80);
+            //TestNeuralNetworkManager(80);
         }
 
         public void TestNeuralNetworkManager(int epochsCount)
@@ -45,6 +46,36 @@ namespace Magisterka
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+        }
+
+        private void btnSubmitLayers_Click(object sender, RoutedEventArgs e)
+        {
+            int positionToInsert = LeftStackPanel.Children.IndexOf(LayersStackPanel);
+            int layersCount;
+
+            if (!Int32.TryParse(tboxLayers.Text, out layersCount))
+                throw new TextBoxParseException("Cannot parse layers textbox");
+            layersTextBoxes = new List<TextBox>();
+
+            for (int i = 0; i < layersCount; i++)
+            {
+                StackPanel tmpStackPanel = new StackPanel();
+                tmpStackPanel.Orientation = Orientation.Horizontal;
+
+                tmpStackPanel.Children.Add(new TextBox
+                {
+                    Name = "layer" + (i + 1),
+                    Height = 20,
+                    Margin = new Thickness(3, 1, 1, 3)
+                });
+                layersTextBoxes.Add(new TextBox
+                {
+                    Name = "layer" + (i + 1),
+                    Height = 20
+                });
+                LeftStackPanel.Children.Insert(positionToInsert + 1, layersTextBoxes[i]);
+            }
+
         }
     }
 }
